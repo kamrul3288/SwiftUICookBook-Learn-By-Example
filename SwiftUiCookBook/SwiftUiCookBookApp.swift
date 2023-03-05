e42917104a175f3b292e4c93dee056f4e8e57669
KamrulHasan <kamrulhasan3288@gmail.com>
2023-03-05 15:30:09 +0600

    Initial Commit

---
+++ SwiftUiCookBook/SwiftUiCookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCookBookApp.swift
+//  SwiftUiCookBook
+//
+//  Created by Kamrul Hasan on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
